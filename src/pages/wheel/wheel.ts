079d98d3acd10fa53ea5e09e71054895b0e0c4e5
Han <wahan9501@gmail.com>
2021-09-04 04:09:05 +0800

    Beta 1.0, wheel page UI polished.

--- src/pages/wheel/wheel.ts
+++ src/pages/wheel/wheel.ts
@@ -1,64 +1,138 @@
 import wheelHtml from "./wheel.html";
-// // import "./wheel.css";
-// import wheelHtml from "./wheel.html";
 
-console.log("wheel script");
-// console.log("wheel html", wheelHtml);
+const SEL_RANGE = 50;
+const POINTER_R = 20;
+const PANEL_R = 80;
+const ARROW_OFFSET = 40;
+const ITEM_DIST = 160;
 
-// const item = { title: "Bing", url: "https://bing.com" };
-// const item2 = { title: "Google", url: "https://google.com" };
-// const item3 = { title: "Baidu", url: "https://baidu.com" };
-// const items = [item, item2, item3, item, item, item, item, item];
+let containerEle;
+let canvasEle;
+let pointerEle;
+let panelEle;
+let panelLightEle;
+let arrowEle;
 
-// let items;
-// if (result && result["wheelItems"]) {
-//   items = result["wheelItems"];
-// } else {
-//   items = new Array(8);
-// }
-let items = new Array(8);
-
-let container;
-let canvas;
-let ctx;
 var animation;
-var tracker;
 
-const WIDTH = 480;
-const HEIGHT = 480;
-const CX = WIDTH / 2;
-const CY = HEIGHT / 2;
-const WHEEL_SEL_R = 70;
-const WHEEL_INNER_R = 100;
-const WHEEL_OUTER_R = 120;
-const POINTER_R = 10;
-const ARROW_SIDE = 20;
-
+let wheelItems;
+let CX = 0;
+let CY = 0;
 let mx = 0;
 let my = 0;
+let m_angle = 0;
 let sel = false;
 let sel_id = -1;
+let wheelItemEles = [];
 
 function reset() {
+  m_angle = 0;
   mx = 0;
   my = 0;
   sel = false;
   sel_id = -1;
 }
 
-function setupKeyboardListener() {
+function drawPanel() {
+  panelEle.style.transform = `translate(${CX - PANEL_R}px, ${CY - PANEL_R}px)`;
+  if (sel) {
+    panelLightEle.style.transform = `rotate(${m_angle}deg)`;
+    panelLightEle.style.display = "block";
+  } else {
+    panelLightEle.style.display = "none";
+  }
+}
+
+function drawPointer() {
+  pointerEle.style.transform = `translate(${CX + mx - POINTER_R}px, ${
+    CY + my - POINTER_R
+  }px)`;
+  if (sel) pointerEle.style.opacity = "0.85";
+  else pointerEle.style.opacity = "0.5";
+}
+
+function drawArrow() {
+  if (sel) {
+    arrowEle.style.transform = `rotate(-45deg)`;
+    arrowEle.style.transform = `translate(${CX}px, ${CY - PANEL_R}px) rotate(${
+      m_angle - 45
+    }deg) translate(${-5}px, ${5}px)`;
+    arrowEle.style.display = "block";
+  } else {
+    arrowEle.style.display = "none";
+  }
+}
+
+function drawWheelItems() {
+  wheelItemEles.forEach((e, i) => {
+    const angle = (45 * i * Math.PI) / 180;
+    let x = CX + ITEM_DIST * Math.sin(angle);
+    let y = CY - ITEM_DIST * Math.cos(angle);
+    e.style.transform = `translate(${x}px, ${y}px)`;
+
+    e.firstChild.className = "wheel-item-text";
+    e.firstChild.style.fontSize = "2rem";
+  });
+
+  if (sel) {
+    wheelItemEles[sel_id].firstChild.className =
+      "wheel-item-text wheel-item-text-selected";
+    wheelItemEles[sel_id].firstChild.style.fontSize = "3rem";
+  }
+}
+
+function draw() {
+  drawPanel();
+  drawPointer();
+  drawArrow();
+  drawWheelItems();
+}
+
+function update() {
+  if (!animation) {
+    animation = requestAnimationFrame(function () {
+      animation = null;
+      draw();
+    });
+  }
+}
+
+function updatePosition(e) {
+  mx += e.movementX;
+  my += e.movementY;
+  let mag = Math.sqrt(mx * mx + my * my);
+
+  const clamp = PANEL_R - POINTER_R;
+  if (mag > clamp) {
+    mx *= clamp / mag;
+    my *= clamp / mag;
+    mag = clamp;
+  }
+
+  m_angle = (Math.atan2(my, mx) * 180) / Math.PI;
+  m_angle = (m_angle + 360 + 90) % 360;
+
+  if (mag > SEL_RANGE) {
+    sel = true;
+    sel_id = Math.floor(((m_angle + 22.5) % 360) / 45);
+  } else {
+    sel = false;
+  }
+
+  update();
+}
+
+function addKeyboardListener() {
   document.addEventListener(
     "keydown",
     (event) => {
       if (event.altKey && event.key === "s") {
-        console.log(">> keydown");
-        // event.preventDefault();
-        if (canvas.style.display === "none") {
+        if (containerEle.style.display === "none") {
           reset();
           draw();
         }
-        container.style.display = "flex";
-        canvas.requestPointerLock();
+        containerEle.style.display = "flex";
+        canvasEle.requestPointerLock();
       }
     },
     true
@@ -68,7 +142,7 @@ function setupKeyboardListener() {
     "keyup",
     (event) => {
       if (event.key === "Alt" || event.key === "s") {
-        container.style.display = "none";
+        containerEle.style.display = "none";
         document.exitPointerLock();
       }
     },
@@ -76,145 +150,14 @@ function setupKeyboardListener() {
   );
 }
 
-function drawWheel() {
-  ctx.save();
-
-  ctx.translate(CX, CY);
-
-  ctx.strokeStyle = sel ? "#0f0" : "#000";
-  ctx.beginPath();
-  ctx.arc(0, 0, WHEEL_INNER_R, 0, Math.PI * 2, true);
-  ctx.stroke();
-
-  ctx.beginPath();
-  ctx.arc(0, 0, WHEEL_OUTER_R, 0, Math.PI * 2, true);
-  ctx.stroke();
-
-  ctx.restore();
-}
-
-function drawArrow() {
-  ctx.save();
-
-  ctx.translate(CX, CY);
-  ctx.rotate((45 * sel_id * Math.PI) / 180);
-  ctx.translate(0, -WHEEL_OUTER_R - ARROW_SIDE);
-
-  ctx.fillStyle = "#0f0";
-
-  ctx.beginPath();
-  const h = ARROW_SIDE * (Math.sqrt(3) / 2);
-  ctx.moveTo(0, (-h * 2) / 3);
-  ctx.lineTo((h * 2) / 3, h / 3);
-  ctx.lineTo((-h * 2) / 3, h / 3);
-  ctx.closePath();
-  ctx.fill();
-
-  ctx.restore();
-}
-
-function drawItems() {
-  ctx.save();
-  ctx.translate(CX, CY);
-
-  // ctx.translate(0, -WHEEL_OUTER_R - ARROW_SIDE - ARROW_SIDE);
-
-  for (let i = 0; i < 8; i++) {
-    ctx.save();
-    ctx.fillStyle = "black";
-    ctx.font = "16px Arial";
-    ctx.textBaseline = "middle";
-    ctx.textAlign = "center";
-    if (sel && i === sel_id) {
-      ctx.fillStyle = "red";
-      ctx.font = "24px Arial";
-    }
-    if (i > 0 && i < 4) {
-      ctx.textAlign = "left";
-    } else if (i > 4 && i < 8) {
-      ctx.textAlign = "right";
-    }
-    const angle = (45 * i * Math.PI) / 180;
-    const len = WHEEL_OUTER_R + ARROW_SIDE * 2;
-    const y = len * Math.cos(angle);
-    const x = len * Math.sin(angle);
-
-    // tracker.textContent = "angle: " + angle + "   len: " + len;
-    // tracker.textContent = "x: " + x + "   y: " + y;
-
-    ctx.translate(x, -y);
-    ctx.fillText(items[i]?.title ?? "N/A", 0, 0);
-
-    ctx.restore();
-  }
-
-  ctx.restore();
-}
-
-function drawPointer() {
-  ctx.save();
-
-  ctx.translate(CX, CY);
-
-  ctx.fillStyle = "#f00";
-  ctx.beginPath();
-  ctx.arc(mx, my, POINTER_R, 0, Math.PI * 2, true);
-  ctx.fill();
-
-  ctx.restore();
-}
-
-function draw() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  drawWheel();
-  drawPointer();
-  if (sel) drawArrow();
-  drawItems();
-
-  // tracker.textContent = "Sel? : " + sel + "   Id: " + sel_id;
-}
-
-function updatePosition(e) {
-  mx += e.movementX;
-  my += e.movementY;
-  let mag = Math.sqrt(mx * mx + my * my);
-
-  const clamp = WHEEL_INNER_R - POINTER_R;
-  if (mag > clamp) {
-    mx *= clamp / mag;
-    my *= clamp / mag;
-    mag = clamp;
-  }
-
-  let angle = (Math.atan2(my, mx) * 180) / Math.PI;
-  angle = (angle + 360 + 90 + 22.5) % 360;
-
-  if (mag > WHEEL_SEL_R) {
-    sel = true;
-    sel_id = Math.floor(angle / 45);
-  } else {
-    sel = false;
-  }
-
-  if (!animation) {
-    animation = requestAnimationFrame(function () {
-      animation = null;
-      draw();
-    });
-  }
-}
-
-function setupMouseListener() {
+function addMouseListener() {
   function lockChangeAlert() {
-    if (document.pointerLockElement === canvas) {
-      // console.log("The pointer lock status is now locked");
+    if (document.pointerLockElement === canvasEle) {
       document.addEventListener("mousemove", updatePosition, false);
     } else {
-      console.log("The pointer lock status is now unlocked");
       if (sel) {
-        // if (sel_id > 0 && sel_id < 8 && items[sel_id] && )
-
-        if (items[sel_id]?.url) window.open(items[sel_id].url, "_blank");
+        if (wheelItems[sel_id]?.url)
+          window.open(wheelItems[sel_id].url, "_blank");
       }
       document.removeEventListener("mousemove", updatePosition, false);
     }
@@ -222,22 +165,128 @@ function setupMouseListener() {
   document.addEventListener("pointerlockchange", lockChangeAlert, false);
 }
 
-function init() {
-  if (!document.getElementById("wheel-container")) {
-    document.body.insertAdjacentHTML("beforebegin", wheelHtml);
-  }
-  container = document.getElementById("wheel-container");
-  canvas = document.getElementById("tutorial");
-  // if (!canvas?.getContext) throw new Error("Not support canvas");
-  ctx = canvas.getContext("2d");
-  tracker = document.getElementById("tracker");
-
-  setupKeyboardListener();
-  setupMouseListener();
-
-  draw();
+function updateCenterPoint(WIDTH, HEIGHT) {
+  CX = WIDTH;
+  CY = HEIGHT;
+  update();
 }
 
-init();
+function addResizeListener() {
+  window.addEventListener("resize", (e) => {
+    updateCenterPoint(
+      (e.currentTarget as Window).innerWidth / 2,
+      (e.currentTarget as Window).innerHeight / 2
+    );
+  });
+}
 
-// console.log(">>", document.getElementById("wheel-container"));
+function initPanel() {
+  panelLightEle = document.createElement("div");
+  panelLightEle.className = "wheel-panel-light";
+  panelLightEle.style.width = `${PANEL_R * 2}px`;
+  panelLightEle.style.height = `${PANEL_R * 2}px`;
+  panelLightEle.style.display = "none";
+
+  panelEle = document.createElement("div");
+  panelEle.className = "wheel-panel";
+  panelEle.style.width = `${PANEL_R * 2}px`;
+  panelEle.style.height = `${PANEL_R * 2}px`;
+  panelEle.style.transform = `translate(${CX - PANEL_R}px, ${CY - PANEL_R}px)`;
+  panelEle.appendChild(panelLightEle);
+
+  panelEle.appendChild(panelLightEle);
+  containerEle.appendChild(panelEle);
+}
+
+function initPointer() {
+  pointerEle = document.createElement("div");
+  pointerEle.className = "wheel-pointer";
+  pointerEle.style.width = `${POINTER_R * 2}px`;
+  pointerEle.style.height = `${POINTER_R * 2}px`;
+  pointerEle.style.boxShadow = `inset ${-POINTER_R / 3}px ${-POINTER_R / 3}px ${
+    (POINTER_R * 2) / 3
+  }px  rgba(0,0,0,0.6), 1px 1px 5px #3e3e3e`;
+  containerEle.appendChild(pointerEle);
+}
+
+function initArrow() {
+  arrowEle = document.createElement("div");
+  arrowEle.className = "wheel-arrow";
+  arrowEle.style.width = `${PANEL_R}px`;
+  arrowEle.style.height = `${PANEL_R}px`;
+  arrowEle.style.clipPath = `path("M ${ARROW_OFFSET} 0 A ${PANEL_R} ${PANEL_R}, 0, 0, 1, ${PANEL_R} ${
+    PANEL_R - ARROW_OFFSET
+  } L ${PANEL_R} 0 Z")`;
+  arrowEle.style.display = "none";
+
+  containerEle.appendChild(arrowEle);
+}
+
+function initWheelItems(items) {
+  wheelItems = items ?? new Array(8);
+
+  for (let i = 0; i < 8; i++) {
+    let wheelItemText = document.createElement("div");
+    wheelItemText.className = "wheel-item-text";
+    wheelItemText.innerText = wheelItems[i]?.title ?? "N/A";
+
+    let wheelItem = document.createElement("div");
+    wheelItem.className = "wheel-item";
+    wheelItem.append(wheelItemText);
+
+    const angle = (45 * i * Math.PI) / 180;
+    let x = CX + ITEM_DIST * Math.sin(angle);
+    let y = CY - ITEM_DIST * Math.cos(angle);
+    wheelItem.style.transform = `translate(${x}px, ${y}px)`;
+
+    if (i === 0 || i === 4) {
+      wheelItemText.style.textAlign = "center";
+      wheelItemText.style.transform = "translate(-50%, -50%)";
+    } else if (i > 0 && i < 4) {
+      wheelItemText.style.textAlign = "left";
+      wheelItemText.style.transform = "translate(0%, -50%) translateX(-2em)";
+    } else if (i > 4 && i < 8) {
+      wheelItemText.style.textAlign = "right";
+      wheelItemText.style.transform = "translate(-100%, -50%) translateX(2em)";
+    }
+
+    wheelItemEles.push(wheelItem);
+    containerEle.append(wheelItem);
+  }
+}
+
+function init(wheelItems) {
+  if (!document.getElementById("wheel-container")) {
+    document.body.insertAdjacentHTML("beforebegin", wheelHtml);
+    containerEle = document.getElementById("wheel-container");
+    canvasEle = document.querySelector("canvas.wheel-canvas");
+
+    // TODO remove listener?
+    addKeyboardListener();
+    addMouseListener();
+    addResizeListener();
+
+    updateCenterPoint(window.innerWidth / 2, window.innerHeight / 2);
+
+    initWheelItems(wheelItems);
+    initPanel();
+    initPointer();
+    initArrow();
+
+    draw();
+  }
+}
+
+const key = "wheelItems";
+chrome.storage.local.get(key, (result) => {
+  let items;
+  if (result && result[key]) {
+    items = result[key];
+  } else {
+    items = new Array(8);
+  }
+
+  console.log(">>>>", result, items);
+
+  init(items);
+});
--- src/scripts/background.ts
+++ src/scripts/background.ts
@@ -1,18 +1,16 @@
 console.log("background script");
 
 chrome.tabs.onUpdated.addListener(async function (tabId, changeInfo, tab) {
-  //code in here will run every time a user goes onto a new tab, so you can insert your scripts into every new tab
-  // tab.
-  // reddenPage();
-
-  // loadWheel();
-  // console.log(JSON.stringify(changeInfo));
   if (changeInfo.status === "loading") {
     await chrome.scripting.executeScript({
       target: { tabId: tab.id },
       files: ["wheel.js"],
     });
   }
+});
+
+chrome.action.onClicked.addListener((tab) => {
+  chrome.tabs.create({ url: chrome.runtime.getURL("wheelSettings.html") });
 });
 
 // // chrome://flags/#extensions-on-chrome-url
