711843534750ff2ee11d32c01659f6a2f935a1f8
Han <wahan9501@gmail.com>
2021-09-06 10:03:05 +0800

    temp

--- src/pages/wheelSettings/wheelSettings.ts
+++ src/pages/wheelSettings/wheelSettings.ts
@@ -34,47 +34,56 @@ function validURL(str) {
 
 const key = "wheelItems";
 
-chrome.storage.local.get(key, function (result) {
-  let workItems;
-  if (result && result[key]) {
-    workItems = result[key] as WheelItem[];
-  } else {
-    workItems = new Array(8);
-  }
+// chrome.storage.local.get(key, function (result) {
+//   let workItems;
+//   if (result && result[key]) {
+//     workItems = result[key] as WheelItem[];
+//   } else {
+//     workItems = new Array(8);
+//   }
 
-  for (let i = 0; i < 8; i++) {
-    if (workItems[i]) {
-      const inputItem = document.getElementsByClassName("input-item")[i];
-      const titleInput = inputItem
-        .getElementsByClassName("input-title")[0]
-        .getElementsByTagName("input")[0];
-      titleInput.value = workItems[i].title;
-      const urlInput = inputItem
-        .getElementsByClassName("input-url")[0]
-        .getElementsByTagName("input")[0];
-        urlInput.value = workItems[i].url;
-    }
-  }
+//   for (let i = 0; i < 8; i++) {
+//     if (workItems[i]) {
+//       const inputItem = document.getElementsByClassName("input-item")[i];
+//       const titleInput = inputItem
+//         .getElementsByClassName("input-title")[0]
+//         .getElementsByTagName("input")[0];
+//       titleInput.value = workItems[i].title;
+//       const urlInput = inputItem
+//         .getElementsByClassName("input-url")[0]
+//         .getElementsByTagName("input")[0];
+//         urlInput.value = workItems[i].url;
+//     }
+//   }
 
-  const onSaveBtnClick = () => {
-    for (let i = 0; i < 8; i++) {
-      const inputItem = document.getElementsByClassName("input-item")[i];
-      const title = inputItem
-        .getElementsByClassName("input-title")[0]
-        .getElementsByTagName("input")[0].value;
-      const url = inputItem
-        .getElementsByClassName("input-url")[0]
-        .getElementsByTagName("input")[0].value;
-      if (title && url && validURL(url)) {
-        workItems[i] = { title, url };
-      }
-    }
+//   const onSaveBtnClick = () => {
+//     for (let i = 0; i < 8; i++) {
+//       const inputItem = document.getElementsByClassName("input-item")[i];
+//       const title = inputItem
+//         .getElementsByClassName("input-title")[0]
+//         .getElementsByTagName("input")[0].value;
+//       const url = inputItem
+//         .getElementsByClassName("input-url")[0]
+//         .getElementsByTagName("input")[0].value;
+//       if (title && url && validURL(url)) {
+//         workItems[i] = { title, url };
+//       }
+//     }
 
-    chrome.storage.local.set({ [key]: workItems }, () => {
-      alert("saved!");
-    });
-  };
+//     chrome.storage.local.set({ [key]: workItems }, () => {
+//       alert("saved!");
+//     });
+//   };
 
-  document.getElementById("saveBtn").onclick = onSaveBtnClick;
-  document.getElementById("root").style.display = "block";
-});
+//   document.getElementById("saveBtn").onclick = onSaveBtnClick;
+//   document.getElementById("root").style.display = "block";
+// });
+
+function render() {
+  const wheel = document.createElement("div");
+  wheel.innerText = "Hello World";
+
+  document.body.appendChild(wheel);
+}
+
+// render();
