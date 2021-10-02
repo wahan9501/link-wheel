259e5c8071cad54db215643d8daecaad06cadb14
Han <wahan9501@gmail.com>
2021-10-02 18:47:41 +0800

    wheel settings allow only 1 instance page

--- src/scripts/background.js
+++ src/scripts/background.js
@@ -8,7 +8,14 @@ chrome.tabs.onUpdated.addListener(async function (tabId, changeInfo, tab) {
 });
 
 chrome.action.onClicked.addListener((tab) => {
-  chrome.tabs.create({ url: chrome.runtime.getURL("wheelSettings.html") });
+  const url = chrome.runtime.getURL("wheelSettings.html");
+  chrome.tabs.query({ url }).then((result) => {
+    if (result.length > 0) {
+      chrome.tabs.highlight({ tabs: result[0].index });
+    } else {
+      chrome.tabs.create({ url });
+    }
+  });
 });
 
 // // chrome://flags/#extensions-on-chrome-url
